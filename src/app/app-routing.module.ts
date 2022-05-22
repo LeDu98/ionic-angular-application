e1cfc0a045ad1a29f677473886c434a72598ca6c
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-05-22 19:42:26 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'work-orders',
+    loadChildren: () => import('./work-orders/work-orders.module').then( m => m.WorkOrdersPageModule),
+    //canLoad:[AuthGuard]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'work-orders',
     pathMatch: 'full'
   },
+  {
+    path: 'my-profile',
+    loadChildren: () => import('./my-profile/my-profile.module').then( m => m.MyProfilePageModule)
+  },
+  {
+    path: 'log-in',
+    loadChildren: () => import('./auth/log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  }
+
 ];
 
 @NgModule({
